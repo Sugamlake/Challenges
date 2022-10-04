4668272c2561cca427ebfdf57c93c43f9e5f3998
sugamlake <sugamlake@gmail.com>
2022-10-04 15:06:38 -0300

    start

--- Readme.md
+++ Readme.md
@@ -1,7 +1,11 @@
 # Programming challenges
 
+### "FIZZ BUZZ"
+
+Participate in the challenges that are carried out every week that helps us to practice and improve our code every day.
+
 ## MoureDev Community Code Challenge Repositories
 
-<a><img src="[![javascript.gif](https://i.postimg.cc/J0qyZWwt/javascript.gif)](https://postimg.cc/6485xgpN)" style="height: 50%; width:50%;"/></a>
+<a href ="https://postimg.cc/6485xgpN"><img src="[![javascript.gif](https://i.postimg.cc/J0qyZWwt/javascript.gif)]" style="height: 50%; width:50%;"/></a>
 
 #### You can review these solved challenges in Javascript, Kotlin and other languages
--- Reto-1/Challenge #1.js
+++ Reto-1/Challenge #1.js
@@ -1,18 +1,18 @@
 // # Fizz a buzz
 
-//Write a program that displays the numbers from 1 to 100 on the screen, substituting the multiples of 3 for the word "fizz", the multiples of 5 for "buzz" and the multiples of both, that is, the multiples of 3 and 5 ( or 15), by the word "fizzbuzz".
+// Write a program that displays the numbers from 1 to 100 on the screen, substituting the multiples of 3 for the word "fizz", the multiples of 5 for "buzz" and the multiples of both, that is, the multiples of 3 and 5 ( or 15), by the word "fizzbuzz".
 
-function compararNúmeros() {
-  for (let númeroAComparar = 1; númeroAComparar <= 100; númeroAComparar++) {
-    if (númeroAComparar % 3 == 0 && númeroAComparar % 5 == 0) {
-      console.log(`${númeroAComparar} FizzBuzz`);
-    } else if (númeroAComparar % 3 == 0) {
-      console.log(`${númeroAComparar} Fizz`);
-    } else if (númeroAComparar % 5 == 0) {
-      console.log(`${númeroAComparar} Buzz`);
+function compareNumbers() {
+  for (let numberToCompare = 1; numberToCompare <= 100; numberToCompare++) {
+    if (numberToCompare % 3 == 0 && numberToCompare % 5 == 0) {
+      console.log(`${numberToCompare} FizzBuzz`);
+    } else if (numberToCompare % 3 == 0) {
+      console.log(`${numberToCompare} Fizz`);
+    } else if (numberToCompare % 5 == 0) {
+      console.log(`${numberToCompare} Buzz`);
     } else {
-      console.log(`${númeroAComparar}`);
+      console.log(`${numberToCompare}`);
     }
   }
 }
-console.log(compararNúmeros());
+console.log(compareNumbers());
